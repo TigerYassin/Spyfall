e3e3b8268148724cee66c6ba77bcd4a9f90bbb6a
Yassin Alsahlani <yasnarutoa@gmail.com>
2018-12-24 20:46:09 -0800

    added mdbootstrap

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { Button } from 'reactstrap';
+import { MDBBtn } from "mdbreact";
+
+
 
 export class Buttons extends React.Component {
   render() {
     return (
       <div>
-        <Button color="primary">Oh</Button>{' '}
-        <Button color="secondary">My</Button>{' '}
-        <Button color="success">God.</Button>{' '}
-        <Button color="info"> It</Button>{' '}
-        <Button color="warning">finally</Button>{' '}
-        <Button color="danger">works!</Button>{' '}
+      working
+            <MDBBtn color="primary">Primary</MDBBtn>
+
       </div>
     );
   }
